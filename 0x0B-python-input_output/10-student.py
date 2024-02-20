a1b0167b0ce789d9223741b18136e43d8667ee92
publicenemy-ace <niyisatun@yahoo.com>
2024-02-20 20:30:42 +0000

    python io

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""
+A module defining a class Student
+"""
 
 
 class Student:
-    """Represent a student."""
-
+    """
+    A class defining a student
+    """
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
+        """
+        Initialize instance attributes
+        Args:
+            first_name: the first name of student
+            last_name: the last name of student
+            age: the age of student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student
-        If attrs is a list of strings, represents only those attributes
-        included in the list
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        Instance method returning distinct properties of object
+        """
+        if type(attrs) == list:  # check if it's a list
+            for elem in attrs:
+                if type(elem) != str:  # check if elements are strings
+                    return (self.__dict__)
+
+            dictionary = {}
+            for num in range(len(attrs)):  # iterate thro list
+                for item in self.__dict__:
+                    if attrs[num] == item:
+                        dictionary[item] = (self.__dict__[item])  # add item
+            return (dictionary)
+
+        return (self.__dict__)
